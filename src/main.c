3dac3b87a1bb295d1755dc4c7a488b96d6a833e5
Durfan <31577607+Durfan@users.noreply.github.com>
2019-08-16 18:09:54 -0300

    alpha

--- src/main.c
+++ src/main.c
@@ -3,184 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <linux/limits.h>
 
-#define LSH_RL_BUFSIZE 1024
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
-
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-
-char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit"
-};
-
-int (*builtin_func[]) (char **) = {
-	&lsh_cd,
-	&lsh_help,
-	&lsh_exit
-};
-
-int lsh_num_builtins() {
-	return sizeof(builtin_str) / sizeof(char *);
+void initshell(void) {
+	system("clear");
+	printf("  _____ _    _ ______ _      _      ____   _____    ___  __\n");
+	printf(" / ____| |  | |  ____| |    | |    / __ \\ / ____|  / _ \\/_ |\n");
+	printf("| (___ | |__| | |__  | |    | |   | |  | | (___   | | | || | __ _\n");
+	printf(" \\___ \\|  __  |  __| | |    | |   | |  | |\\___ \\  | | | || |/ _` |\n");
+	printf(" ____) | |  | | |____| |____| |___| |__| |____) | | |_| || | (_| |\n");
+	printf("|_____/|_|  |_|______|______|______\\____/|_____/   \\___(_)_|\\__,_|\n\n");                                                
 }
 
-int lsh_cd(char **args) {
-	if (args[1] == NULL) {
-		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-	} else {
-		if (chdir(args[1]) != 0) {
-		perror("lsh");
-		}
-	}
-	return 1;
+char *inputline(void) {
+	char *line = NULL;
+	ssize_t bufsize = 0;
+	getline(&line, &bufsize, stdin);
+	return line;
 }
 
-int lsh_help(char **args) {
-	int i;
-	printf("Stephen Brennan's LSH\n");
-	printf("Type program names and arguments, and hit enter.\n");
-	printf("The following are built in:\n");
 
-	for (i = 0; i < lsh_num_builtins(); i++) {
-		printf("  %s\n", builtin_str[i]);
-	}
-
-	printf("Use the man command for information on other programs.\n");
-	return 1;
-}
-
-int lsh_exit(char **args) {
-	return 0;
-}
-
-int lsh_launch(char **args) {
-	pid_t pid, wpid;
-	int status;
-
-	pid = fork();
-
-	if (pid == 0) {
-		if (execvp(args[0], args) == -1) {
-		perror("lsh");
-		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0) {
-		perror("lsh");
-	}
-	else {
-		do {
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-
-	return 1;
-}
-
-int lsh_execute(char **args) {
-	int i;
-
-	if (args[0] == NULL) {
-		return 1;
-	}
-
-	for (i = 0; i < lsh_num_builtins(); i++) {
-		if (strcmp(args[0], builtin_str[i]) == 0) {
-			return (*builtin_func[i])(args);
-		}
-	}
-
-	return lsh_launch(args);
-}
-
-char *lsh_read_line(void) {
-	int bufsize = LSH_RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
-
-	if (!buffer) {
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while (1) {
-		c = getchar();
-
-		if (c == EOF || c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
-		}
-		else {
-			buffer[position] = c;
-		}
-		position++;
-
-		if (position >= bufsize) {
-			bufsize += LSH_RL_BUFSIZE;
-			buffer = realloc(buffer, bufsize);
-			if (!buffer) {
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-}
-
-char **lsh_split_line(char *line) {
-	int bufsize = LSH_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token;
-
-	if (!tokens) {
-		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	token = strtok(line, LSH_TOK_DELIM);
-
-	while (token != NULL) {
-		tokens[position] = token;
-		position++;
-
-		if (position >= bufsize) {
-			bufsize += LSH_TOK_BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens) {
-				fprintf(stderr, "lsh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-		token = strtok(NULL, LSH_TOK_DELIM);
-	}
-	tokens[position] = NULL;
-	return tokens;
-}
-
-void lsh_loop(void) {
+void prompt(void) {
+	char *user = getenv("USER");
+	char host[1024];
+	char cwd[PATH_MAX];
 	char *line;
 	char **args;
-	int status;
+
+	gethostname(host,1024);
 
 	do {
-		printf("$ ");
-		line = lsh_read_line();
-		args = lsh_split_line(line);
-		status = lsh_execute(args);
+		if (getcwd(cwd, sizeof(cwd)) != NULL)
+			printf("\u250C\u2574%s\n", cwd);
+		
+		printf("\u2514\u2574[%s@%s]$ ", user, host);
+		line = inputline();
 
 		free(line);
-		free(args);
-	} while (status);
+	} while (1);
 }
+
 
 int main(int argc, char **argv) {
 
-	lsh_loop();
+	initshell();
+	prompt();
 
 	return EXIT_SUCCESS;
 }
